2faa7c8e2a42d71a0879a4e4d5c82eab38f6cc62
Shady <107540007+shadyo6@users.noreply.github.com>
2023-12-27 19:59:20 +0530

    Update d.sh

--- d.sh
+++ d.sh
@@ -2,6 +2,6 @@ alias v='less /tmp/v.txt'
 alias w='open /tmp/NPwriteup.pdf'
 alias c='history -c'
 alias d='rm /tmp/NPwriteup.pdf /tmp/v.txt'
-alias getv='sftp lab2@10.11.1.121:/C:/wwampserver_/v.txt /tmp'
-alias getw='sftp lab2@10.11.1.121:/C:/wwampserver_/NPwriteup.pdf /tmp'
+alias getv='sftp lab2@10.11.1.121:/C:/wwampserver_/v.txt /tmp/'
+alias getw='sftp lab2@10.11.1.121:/C:/wwampserver_/NPwriteup.pdf /tmp/'
 
